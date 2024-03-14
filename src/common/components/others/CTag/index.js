ea4c79a1c9bea489a04883d3b8594af0e507c72f
phucdh410 <phucdh410@gmail.com>
2024-03-14 14:46:41 +0700

    Update css

--- src/common/components/others/CTag/index.js
+++ src/common/components/others/CTag/index.js
@@ -1,16 +1,17 @@
-export const CTag = ({ label, color = "#98ffb6" }) => {
+export const CTag = ({ label, color = "rgb(189 255 208)" }) => {
   return (
     <div
       style={{
         display: "inline-flex",
         alignItems: "center",
         justifyContent: "enter",
-        borderRadius: "12px",
+        borderRadius: "8px",
         paddingInline: "12px",
         paddingBlock: "6px",
-        backgroundColor: color || "#98ffb6",
+        backgroundColor: color || "rgb(189 255 208)",
         fontWeight: 500,
         fontSize: 14,
+        boxShadow: "0 0 4px 2px rgb(0 0 0 / 12%)",
       }}
     >
       {label}
