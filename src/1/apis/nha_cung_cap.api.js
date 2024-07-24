9b9ba0df4adc70b8c99b294c54902e2da33943c3
phucdh410 <phucdh410@gmail.com>
2024-07-24 15:17:43 +0700

    update endpoint

--- src/1/apis/nha_cung_cap.api.js
+++ src/1/apis/nha_cung_cap.api.js
@@ -2,18 +2,18 @@ import { methods } from "../axios";
 
 export const nhaCungCapApi = {
   createSuggest: async (body) => {
-    return await methods.post("/suppliers", body);
+    return await methods.post("/supplier-proposals", body);
   },
   getAllSuggest: async (params) => {
-    return await methods.get("/suppliers", { params });
+    return await methods.get("/supplier-proposals", { params });
   },
   getDetailSuggest: async (id) => {
-    return await methods.get(`/suppliers/${id}`);
+    return await methods.get(`/supplier-proposals/${id}`);
   },
   updateSuggest: async (id, body) => {
-    return await methods.put(`/suppliers/${id}`, body);
+    return await methods.put(`/supplier-proposals/${id}`, body);
   },
   removeSuggest: async (id) => {
-    return await methods.delete(`/suppliers/${id}`);
+    return await methods.delete(`/supplier-proposals/${id}`);
   },
 };
